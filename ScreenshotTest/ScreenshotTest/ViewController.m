bf3ee01e71efc9be31ea94de7f80834317d0ebcf
huangjian <huangjian@mxchip.com>
2018-12-24 15:46:45 +0800

    ..

--- ScreenshotTest/ScreenshotTest/ViewController.m
+++ ScreenshotTest/ScreenshotTest/ViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(screenShoting:) name:UIApplicationUserDidTakeScreenshotNotification object:nil];
-    /// 我的个乖乖
 }
 -(void)screenShoting:(NSNotification *)noti
 {
